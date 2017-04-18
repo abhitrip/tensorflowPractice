de10bd4d8d03f5881078f888c3cf06d097b4ac1e
abhitrip <atripath@eng.ucsd.edu>
2017-04-18 12:45:43 -0700

    fully working mnist cnn

--- conv_toy.py
+++ conv_toy.py
@@ -6,3 +6,42 @@
 # padding = 'SAME'
 # conv = tf.nn.conv2d(input,filter_weights,strides,padding)+filter_bias
 
+import tensorflow as tf
+# output depth
+k_output = 64
+
+# Image properties
+image_width = 10
+image_height = 10
+color_channels = 3
+
+# Convolution filter
+filter_size_width = 5
+filter_size_height = 5
+
+# Input/Image
+input = tf.placeholder(tf.float32,shape=[None,image_height,image_width,
+                                        image_width,color_channels])
+
+# Weight and bias
+Weight = tf.Variable(tf.truncated_normal([filter_size_height,
+                                        filter_size_width,color_channels
+                                        ,k_output]))
+bias = tf.Variable(tf.zeros(k_output))
+
+# Apply convolution
+conv_layer = tf.nn.conv2d(input,Weight,strides=[1,2,2,1],padding='SAME')
+
+# Add bias
+conv_layer = tf.nn.bias_add(conv_layer,bias)
+
+# Apply activation function
+conv_layer = tf.nn.relu(conv_layer)
+
+# Apply pooling
+conv_layer = tf.nn.max_pool(
+             conv_layer,
+             ksize=[1,2,2,1],
+             strides=[1,2,2,1],
+             padding='SAME'
+            )
